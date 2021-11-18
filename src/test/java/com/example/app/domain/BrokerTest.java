49eae0951f2a36c2cdeea744af374a5f07711012
KonstBet <kostasbetchavas@hotmail.com>
2021-11-18 19:26:55 +0200

    BrokerTest not completed need sellStocksForInvestor

--- src/test/java/com/example/app/domain/BrokerTest.java
+++ src/test/java/com/example/app/domain/BrokerTest.java
@@ -1,8 +1,11 @@
 package com.example.app.domain;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import java.time.LocalDateTime;
+import java.util.HashSet;
 
 public class BrokerTest {
     private Investor investor;
@@ -12,6 +15,8 @@ public class BrokerTest {
     private Stock CosmoteStock;
     private Integer amount;
     private StockHolding sh;
+    private LocalDateTime date1;
+    private LocalDateTime date2;
 
     @BeforeEach
     public void setUpTests() {
@@ -25,5 +30,57 @@ public class BrokerTest {
         amount = 10;
         sh = new StockHolding(amount, PeiraiosStock, investor);
         investor.addStockHolding(PeiraiosStock, sh);
+
+        date1 = LocalDateTime.of(2021,12,31,0,0,0);
+        date2 = LocalDateTime.of(2021,12,31,0,0,0);
     }
+
+    @Test
+    public void buyStocksForInvestorTest() {
+        investor.giveCapitalAuthorization(500.00,broker,date1);
+
+        HashSet<Authorization> auths = (HashSet<Authorization>) investor.getAuthorizations();
+        AuthCapital ac = (AuthCapital) auths.iterator().next();
+
+        boolean flag = broker.buyStocksForInvestor(ac, PeiraiosStock, 10);
+
+        Assertions.assertTrue(flag);
+    }
+
+    @Test
+    public void BrokenbuyStocksForInvestorTest() {
+        investor.giveCapitalAuthorization(500.00,broker,date1);
+
+        HashSet<Authorization> auths = (HashSet<Authorization>) investor.getAuthorizations();
+        AuthCapital ac = (AuthCapital) auths.iterator().next();
+
+        boolean flag = broker.buyStocksForInvestor(ac, PeiraiosStock, 500);
+
+        Assertions.assertFalse(flag);
+    }
+
+//TODO CANT RUN CAUSE NEED sellStocksForInvestor IMPLEMENTATION
+//    @Test
+//    public void sellStocksForInvestorTest() {
+//        investor.giveStocksAuthorization(10,sh,broker,date1);
+//
+//        HashSet<Authorization> auths = (HashSet<Authorization>) investor.getAuthorizations();
+//        AuthStocks ac = (AuthStocks) auths.iterator().next();
+//
+//        boolean flag = broker.sellStocksForInvestor(ac, 10);
+//
+//        Assertions.assertTrue(flag);
+//    }
+//
+//    @Test
+//    public void BrokensellStocksForInvestorTest() {
+//        investor.giveStocksAuthorization(10,sh,broker,date1);
+//
+//        HashSet<Authorization> auths = (HashSet<Authorization>) investor.getAuthorizations();
+//        AuthStocks ac = (AuthStocks) auths.iterator().next();
+//
+//        boolean flag = broker.sellStocksForInvestor(ac, 500);
+//
+//        Assertions.assertFalse(flag);
+//    }
 }
