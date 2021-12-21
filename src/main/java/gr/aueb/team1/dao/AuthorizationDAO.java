d5c3f842360d3758e07a0d5fb899684acace72d3
KonstBet <kostasbetchavas@hotmail.com>
2021-12-21 23:19:23 +0200

    authDAO findcapital and findstock authss

--- src/main/java/gr/aueb/team1/dao/AuthorizationDAO.java
+++ src/main/java/gr/aueb/team1/dao/AuthorizationDAO.java
@@ -1,8 +1,6 @@
 package gr.aueb.team1.dao;
 
-import gr.aueb.team1.domain.AuthCapital;
-import gr.aueb.team1.domain.AuthStock;
-import gr.aueb.team1.domain.Authorization;
+import gr.aueb.team1.domain.*;
 
 import java.util.List;
 
@@ -10,5 +8,7 @@ public interface AuthorizationDAO extends DAO<Authorization> {
     public List<Authorization> findAllByBrokerID(Integer id);
     public AuthStock findStockAuthById(Integer id);
     public AuthCapital findCapitalAuthById(Integer id);
+    public AuthStock findStockAuth(Investor investor, Broker broker, Stock stock);
+    public AuthStock findCapitalAuth(Investor investor,Broker broker);
     public List<Authorization> findAllByInvestorID(Integer id);
 }
