258adde0e1c64da4c8c69809108f85b828e345b9
KonstBet <kostasbetchavas@hotmail.com>
2022-03-19 18:38:04 +0200

    Added SOA folder and basic quarkus app for WalletService

---
+++ SOA/WalletService/src/test/java/org/acme/NativeWalletServiceIT.java
@@ -0,0 +1,9 @@
+package org.acme;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeWalletServiceIT extends WalletServiceTest {
+
+    // Execute the same tests but in native mode.
+}
