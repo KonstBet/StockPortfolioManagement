a0907535af408bce019316270f8698887c41b5de
KonstBet <kostasbetchavas@hotmail.com>
2022-04-27 21:21:43 +0300

    Done metrics and faulttolerance?

--- SOA/WalletService/src/main/java/org/acme/resources/TransactionResource.java
+++ SOA/WalletService/src/main/java/org/acme/resources/TransactionResource.java
@@ -3,6 +3,11 @@ import io.quarkus.hibernate.orm.runtime.graal.service.jacc.Delete_StandardJaccSe
 import org.acme.domain.Transaction;
 import org.acme.repositories.TransactionRepository;
 import org.acme.services.TransactionService;
+import org.eclipse.microprofile.faulttolerance.Retry;
+import org.eclipse.microprofile.faulttolerance.Timeout;
+import org.eclipse.microprofile.metrics.MetricUnits;
+import org.eclipse.microprofile.metrics.annotation.Counted;
+import org.eclipse.microprofile.metrics.annotation.Timed;
 
 import javax.inject.Inject;
 import javax.transaction.Transactional;
@@ -15,12 +20,16 @@ import java.util.List;
 @Path("/transaction")
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
+@Retry(maxRetries = 3)
+@Timeout(2000)
 public class TransactionResource {
 
     @Inject
     TransactionService transactionService;
 
     @GET
+    @Counted(name = "transactionListRequestsCounter", description = "How many requests for transaction list has been performed.")
+    @Timed(name = "transactionListTimer", description = "A measure of how long it takes to perform a transactionList request.", unit = MetricUnits.MILLISECONDS)
     public Response list(@QueryParam("user_id") Long userId) {
         try {
             List<TransactionDTO> transactionDTOList = transactionService.list(userId);
@@ -30,12 +39,14 @@ public class TransactionResource {
             return Response.ok(transactionDTOList).build();
         }
         catch(Exception e) {
-            return null;
+            return Response.status(404).build();
         }
     }
 
     @GET
     @Path("/{id}")
+    @Counted(name = "transactionGetRequestsCounter", description = "How many requests for transaction get has been performed.")
+    @Timed(name = "transactionGetTimer", description = "A measure of how long it takes to perform a transactionGet request.", unit = MetricUnits.MILLISECONDS)
     public Response get(@PathParam("id") Long id) {
         try {
             TransactionDTO transactionDTO = transactionService.get(id);
@@ -44,12 +55,14 @@ public class TransactionResource {
             return Response.ok(transactionDTO).build();
         }
         catch(Exception e) {
-            return null;
+            return Response.status(404).build();
         }
     }
 
     @POST
     @Transactional
+    @Counted(name = "transactionCreateRequestsCounter", description = "How many requests for transaction creation has been performed.")
+    @Timed(name = "transactionCreateTimer", description = "A measure of how long it takes to perform a transactionCreate request.", unit = MetricUnits.MILLISECONDS)
     public Response create(TransactionDTO t) {
         try {
             Long id = transactionService.create(t);
--- SOA/WalletService/src/main/java/org/acme/resources/WalletResource.java
+++ SOA/WalletService/src/main/java/org/acme/resources/WalletResource.java
@@ -3,6 +3,11 @@ package org.acme.resources;
 import org.acme.domain.Transaction;
 import org.acme.domain.Wallet;
 import org.acme.services.WalletService;
+import org.eclipse.microprofile.faulttolerance.Retry;
+import org.eclipse.microprofile.faulttolerance.Timeout;
+import org.eclipse.microprofile.metrics.MetricUnits;
+import org.eclipse.microprofile.metrics.annotation.Counted;
+import org.eclipse.microprofile.metrics.annotation.Timed;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
@@ -14,6 +19,8 @@ import javax.ws.rs.core.Response;
 @Path("/balance")
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
+@Retry(maxRetries = 3)
+@Timeout(2000)
 public class WalletResource {
 
     @Inject
@@ -21,6 +28,8 @@ public class WalletResource {
 
     @GET
     @Path("/{id}")
+    @Counted(name = "BalanceGetRequestsCounter", description = "How many requests for balance get has been performed.")
+    @Timed(name = "BalanceGetTimer", description = "A measure of how long it takes to perform a balanceGet request.", unit = MetricUnits.MILLISECONDS)
     public Response get(@PathParam("id") Long id) {
         try {
             Wallet wallet = walletService.get(id);
@@ -37,13 +46,15 @@ public class WalletResource {
             return Response.ok(walletDTO).build();
         }
         catch(Exception e) {
-            return null;
+            return Response.status(404).build();
         }
     }
 
     @PUT
     @Path("")
     @Transactional
+    @Counted(name = "BalanceUpdateRequestsCounter", description = "How many requests for balance update has been performed.")
+    @Timed(name = "BalanceUpdateTimer", description = "A measure of how long it takes to perform a balanceUpdate request.", unit = MetricUnits.MILLISECONDS)
     public Response update(WalletDTO walletDTO) {
         try {
 
@@ -52,7 +63,7 @@ public class WalletResource {
             return Response.ok().build();
         }
         catch(Exception e) {
-            return null;
+            return Response.status(400).build();
         }
     }
 
